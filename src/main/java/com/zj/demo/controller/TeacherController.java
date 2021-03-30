26685ecb590b2964a33481e2a25b5bbc34ff1d31
hanjizhou98 <1932146124@qq.com>
2021-03-30 10:51:51 +0800

    paike

--- src/main/java/com/zj/demo/controller/TeacherController.java
+++ src/main/java/com/zj/demo/controller/TeacherController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Controller
